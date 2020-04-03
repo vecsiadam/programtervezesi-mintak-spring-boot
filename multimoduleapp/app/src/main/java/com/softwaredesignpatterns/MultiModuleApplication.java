cb1605db89370a87e280eb88891e3d7e8656b1d6
Adam Vecsi <adamvecsi94@gmail.com>
2020-04-03 08:02:10 +0200

    application name refactor

--- multimoduleapp/app/src/main/java/com/softwaredesignpatterns/MultiModuleApplication.java
+++ multimoduleapp/app/src/main/java/com/softwaredesignpatterns/MultiModuleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SoftwareDesignPatternsApplication {
+public class MultiModuleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SoftwareDesignPatternsApplication.class, args);
+		SpringApplication.run(MultiModuleApplication.class, args);
 	}
 
 }
